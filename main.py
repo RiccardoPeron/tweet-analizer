bab24944f98e3ac7da04b952c5aba75742782221
RiccardoPeron <riccardo.peron@studenti.unitn.it>
2022-02-17 10:41:43 +0100

    :building_construction: Resructured main

--- main.py
+++ main.py
@@ -9,45 +9,8 @@ from nltk.corpus import stopwords
 from tqdm import tqdm
 
 # import tweet_analyzer
-# import tweet_preprocesser
-# from tweet_analyzer import TweetAnalyzer as TA
-# from tweet_preprocesser import TweetPreprocessing as TP
-
-# tweet_analyzer.analyze('tweet_from_2016_to_2020.json')
-
-
-def wrap_tweet(outname):
-    tweet_total = []
-
-    path = os.getcwd() + "/JSON/"
-    for dir in os.listdir(path):
-        filedir = path + "/" + dir
-        files = []
-        lengths = []
-        index = []
-        file_number = 0
-        for file in os.listdir(filedir):
-            f = json.load(open(filedir + "/" + file))
-            files.append(f)
-            lengths.append(len(f))
-            index.append(0)
-            file_number += 1
-
-        tot_tweets = sum(lengths)
-        for i in tqdm(range(tot_tweets)):
-            tweets = []
-            for j in range(file_number):
-                if index[j] < lengths[j]:
-                    tweets.append(files[j][index[j]]["created_at"])
-                else:
-                    tweets.append("9999-12-31T09:12:16+00:00")
-
-            idx = tweets.index(min(tweets))
-            tweet_total.append(files[idx][index[idx]])
-            index[idx] += 1
-
-    with open("{}.json".format(outname), "w") as outfile:
-        json.dump(tweet_total, outfile)
+import tweet_preprocesser as TP
+import relate_word_explorer as TE
 
 
 def check_wrap(filename):
@@ -59,9 +22,20 @@ def check_wrap(filename):
         print(" >>> ERROR <<< ")
 
 
-wrap_tweet("tweet_from_2016_to_2020")
-# check_wrap("tweet_from_2016_to_2020.json")
-
-
 # analizer = TA(open("tweet_from_2016_to_2020.json"))
 # analizer.print_trends_graph('prova', [['brexit'], ['piazza affari'], ['borsa'], ['brexit', 'accordo'], ['brexit', 'ue'], ['brexit', 'sterlina'], ['brexit', 'may'], ['brexit', 'dopo'], ['brexit', 'no'], ['brexit', 'borse'], ['brexit', 'piazza affari'], ['brexit', 'hard']])
+
+merged_file_name = TP.preprocess("sources_it", "it")
+TE.explore2(
+    merged_file_name,
+    "it",
+    [
+        "covid-19",
+        "covid",
+        "covid 19",
+        "coronavirus",
+        "corona virus",
+        "pandemia",
+        "epidemia",
+    ],
+)
